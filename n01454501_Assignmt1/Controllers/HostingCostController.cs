c3ab0c9e3641f2a8a080ed98cfc43c308c8b08c8
Neil Moras <neilmoras@gmail.com>
2021-02-10 17:46:02 -0500

    hostingcost changes

--- n01454501_Assignmt1/Controllers/HostingCostController.cs
+++ n01454501_Assignmt1/Controllers/HostingCostController.cs
@@ -8,7 +8,32 @@ using System.Web.Http;
 namespace n01454501_Assignmt1.Controllers
 {
     public class HostingCostController : ApiController
-    {
+    {/// <summary>
+     ///  Add the cost per fortnite to the     
+     /// </summary>
+     /// <param name="id"></param>
+     /// <returns></returns>
+        public IEnumerable<string> Get(int id)
+
+
+        {
+            double costPerFortnite = 5.50;
+            int daysPerFortnite = 14;
+            double totalDays = id / daysPerFortnite ;
+            double hst = 13;
+
+            double totalCost = costPerFortnite + totalDays;
+            double hstSpecific = totalCost * hst/100;
+            double totalAfterHst = totalCost + hstSpecific;
+            double  hstSpecificRound = hstSpecific;
+
+            return new string[] { "1 fortnite at $5.50/FN = $" + totalCost + " CAD", "HST 13% = $" + hstSpecificRound + " CAD", "Total =  $" + totalAfterHst + " CAD" };
+
+
+
+        }
+
+
 
 
     }
