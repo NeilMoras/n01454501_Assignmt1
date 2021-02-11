1ddb28eca37a0d872b6d9110f6283c0bb0063d51
Neil Moras <neilmoras@gmail.com>
2021-02-10 20:07:04 -0500

    question 6 finally complete, comments left

--- n01454501_Assignmt1/Controllers/HostingCostController.cs
+++ n01454501_Assignmt1/Controllers/HostingCostController.cs
@@ -19,17 +19,20 @@ namespace n01454501_Assignmt1.Controllers
         {
             double costPerFortnite = 5.50;
             int daysPerFortnite = 14;
-            double totalDays = id / daysPerFortnite ;
+            double totalDays = id / daysPerFortnite;
+            totalDays++;
             double hst = 13;
 
-            double totalCost = costPerFortnite + totalDays;
+            double totalCost = costPerFortnite * totalDays ;
             double hstSpecific = totalCost * hst/100;
             double totalAfterHst = totalCost + hstSpecific;
-            double  hstSpecificRound = hstSpecific;
+            double  hstSpecificRound = Math.Round(hstSpecific, 2);
+            double totalCostRound = Math.Round(totalCost, 2);
+            double totalAfterHstRound = Math.Round(totalAfterHst, 2);
 
-            return new string[] { "1 fortnite at $5.50/FN = $" + totalCost + " CAD", "HST 13% = $" + hstSpecificRound + " CAD", "Total =  $" + totalAfterHst + " CAD" };
+            return new string[] { "1 fortnite at $5.50/FN = $" + totalCostRound + " CAD", "HST 13% = $" + hstSpecificRound + " CAD", "Total =  $" + totalAfterHstRound + " CAD" };
 
-
+        
 
         }
 
